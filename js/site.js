0d5ee75003a0c9adc3a9592f8d20a27daefe619d
Ranga Mapuvire <avanna23@gmail.com>
2012-02-21 20:35:29 -0600

    removed nav animation

--- js/site.js
+++ js/site.js
@@ -4,15 +4,14 @@ $(document).ready(function() {
 	
 	$(window).scroll(function(){
 	
-	scrollTop = $(window).scrollTop();
+		scrollTop = $(window).scrollTop();
 	
-	if(scrollTop > 800) {
-		$('#footer').slideDown('slow');
-	} 
-	else {
-		$('#footer').slideUp('slow');
-	}
-	
+			if(scrollTop > 800) {
+				$('#footer').slideDown('slow');
+			} 
+			else {
+				$('#footer').slideUp('slow');
+			}
 	});
 	
 	//load about page with ajax
@@ -55,46 +54,46 @@ $(document).ready(function() {
 	
 	//scrip for navigation cloning and hiding
 	
-	var clonedEl, el = $('#menu');
-	
-	clonedEl = el;
-	
-	clonedEl.before(el.clone()).addClass("fixed");
-	
-$(window).scroll(function(){
-	
-	var el = $('#menu'),
-	offset	= el.offset(),
-	scrollTop = $(window).scrollTop();
-	
-	if(scrollTop > offset.top) {
-		
-		clonedEl.css({'visibility':'visible'}).find('a').each(function (){
-			$(this).css('color','#ffffff');
-		});
-		//el.addClass("fixed");
-		// el.css({'position':'fixed', 'top': '0', 'z-index' :'4500'})
-		//el.stop().animate({"marginTop": ($(window).scrollTop() + 30) + "px"}, "slow");
-	}
-	
-	else {
-		clonedEl.css('visibility','hidden');
-		//el.removeClass("fixed");
-	}
-	
-}).trigger("scroll");	
+	// var clonedEl, el = $('.main_nav');
+	// 	
+	// 	clonedEl = el;
+	// 	
+	// 	clonedEl.before(el.clone()).addClass("fixed");
+	// 	
+	// $(window).scroll(function(){
+	// 	
+	// 	var el = $('.main_nav'),
+	// 	offset	= el.offset(),
+	// 	scrollTop = $(window).scrollTop();
+	// 	
+	// 	if(scrollTop > offset.top) {
+	// 		
+	// 		clonedEl.css({'visibility':'visible'}).find('a').each(function (){
+	// 			$(this).css('color','#ffffff');
+	// 		});
+	// 		//el.addClass("fixed");
+	// 		// el.css({'position':'fixed', 'top': '0', 'z-index' :'4500'})
+	// 		//el.stop().animate({"marginTop": ($(window).scrollTop() + 30) + "px"}, "slow");
+	// 	}
+	// 	
+	// 	else {
+	// 		clonedEl.css('visibility','hidden');
+	// 		//el.removeClass("fixed");
+	// 	}
+	// 	
+	// }).trigger("scroll");	
 
 
 
-	$('.post img:first').hover(
-		
-		function() {
-			$('.post h2, .post h3').fadeOut("slow");
-		},
-		
-		function() {
-			$('.post h2, .post h3').fadeIn("slow");
-		}
-	);
+	// $('.post img:first').hover(
+	// 		
+	// 		function() {
+	// 			$('.post h2, .post h3').fadeOut("slow");
+	// 		},
+	// 		
+	// 		function() {
+	// 			$('.post h2, .post h3').fadeIn("slow");
+	// 		}
+	// 	);
 	
 });
